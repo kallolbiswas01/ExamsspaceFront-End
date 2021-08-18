9444a2d02d5be3b33ff0396fd94391fa35e41488
Anil Kumar <akumar@nsf.org>
2021-08-18 21:06:19 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { AssessmentComponent } from './assessment/assessment.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'assessment', component: AssessmentComponent }
+
 
 
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,36 @@
 import { Component, OnInit } from "@angular/core";
 import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 import { LoginService } from "../services/login.service";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
   public loginForm : FormGroup;
-  constructor(private fb: FormBuilder, private loginService: LoginService) {}
+  constructor(private fb: FormBuilder, private loginService: LoginService, private router: Router) {}
 
   ngOnInit() {
     this.login();
   }
 
   private login() {
+   
+
     this.loginForm = this.fb.group({
       user_email: ["", Validators.required],
       user_password: ["", Validators.required],
       name : ["csrf"],
+      
+      
     });
   }
   public loginUser() {
+    this.router.navigateByUrl('dashboard');
+
     this.loginService.loginUser(this.loginForm.value).subscribe((data) => {
       console.log('login', data)
     });
