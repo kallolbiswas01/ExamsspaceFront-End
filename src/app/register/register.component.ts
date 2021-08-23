b77cfb1e5bfd00732d43ef08f028a6dbe6819768
Sushil Yadav <sky21088@gmail.com>
2021-08-23 18:33:23 +0530

    signup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public signUpForm : FormGroup;
+  signUpData: any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,private loginService:LoginService) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+      name: ["", Validators.required],
+      confPassword: ["", Validators.required],
+      remember: ["", Validators.required]
+    });
+
+  }
+
+  signUp(){
+    this.loginService.signUpUser(this.signUpForm.value).subscribe((data) => {
+      console.log('login', data)
+      let res = <any>data;
+      if(res.type === "success"){
+       this.signUpData = res.data;
+      }
+    });
   }
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,28 +1,40 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { CommonService } from '../common/common.service';
 import { from } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService extends CommonService {
-  constructor(
-    private http: HttpClient
-  ) { 
+  constructor(private http: HttpClient) {
     super();
   }
 
-  public url = "api/user/login";
+  public loginUrl = 'api/user/login';
+  public signUpUrl = 'api/user/signup';
 
-   /**
+  /**
    * funcation name:
    * craeted by:
    * created date:
    * functionality:
    */
-  public loginUser(reqObj:any){
+  public loginUser(reqObj: any) {
+    return this.http.post(
+      `${this.getAppBaseUrl()}${this.loginUrl}`, reqObj, this.getConfig()
+    );
+  }
 
-    return this.http.post(`${this.getAppBaseUrl()}${this.url}`,reqObj,this.getConfig())
+  /**
+   * funcation name:
+   * craeted by:
+   * created date:
+   * functionality:
+   */
+  public signUpUser(reqObj: any) {
+    return this.http.post(
+      `${this.getAppBaseUrl()}${this.signUpUrl}`, reqObj, this.getConfig()
+    );
   }
 }
