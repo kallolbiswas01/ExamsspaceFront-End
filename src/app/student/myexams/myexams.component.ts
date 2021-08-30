8440ca3cc95ee1eb16180e055986119fd89735e4
Anil Kumar <akumar@nsf.org>
2021-08-30 20:49:46 +0530

    My Exam added.

---
+++ src/app/student/myexams/myexams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myexams',
+  templateUrl: './myexams.component.html',
+  styleUrls: ['./myexams.component.scss']
+})
+export class MyexamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
