9939ae78309545fa62123b704796736c63d3c0da
Anil Kumar <akumar@nsf.org>
2021-08-30 10:32:08 +0530

    UI chagnes on quiz section

--- src/app/assessment/input/input.component.ts
+++ src/app/assessment/input/input.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit, OnChanges {
   @Input() data:any;
--- src/app/assessment/radio/radio.component.ts
+++ src/app/assessment/radio/radio.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 @Component({
   selector: 'app-radio',
   templateUrl: './radio.component.html',
-  styleUrls: ['./radio.component.css']
+  styleUrls: ['./radio.component.scss']
 })
 export class RadioComponent implements OnInit, OnChanges {
   @Input() data:any;
