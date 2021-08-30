e97a3b9ecacdd76fd39d4b1c806b69dead85e918
Avisek435 <abhishekkumarjha.abhinam@gmail.com>
2021-08-30 16:44:47 +0530

    icons added

--- src/app/student/livetest/livetest.component.ts
+++ src/app/student/livetest/livetest.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-livetest',
@@ -8,6 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class LivetestComponent implements OnInit {
 
   constructor() { }
+
+  
+
+  // private getDismissReason(reason: any): string {
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return  `with: ${reason}`;
+  //   }
+  // }
+
+  // triggerModal(content) {
+  //   this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+  //     this.closeModal = `Closed with: ${res}`;
+  //   }, (res) => {
+  //     this.closeModal = `Dismissed ${this.getDismissReason(res)}`;
+  //   });
+  // }
 
   ngOnInit(): void {
   }
