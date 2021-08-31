c7f7d2d505630ae49fd2bb1d6b6d13ff660c220a
Anil Kumar <akumar@nsf.org>
2021-08-31 15:33:44 +0530

    Mentor Dummy Json added.

--- src/app/student/mentors/mentors.component.ts
+++ src/app/student/mentors/mentors.component.ts
@@ -12,4 +12,11 @@ export class MentorsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public mentorList = [
+  {"mentorPic":"/assets/img/mentor1.png", "mentorName":"Micheal John", "location":"Ann Arbor, MI, USA","industory":"Math/Physics", "profileView":"a"},
+  {"mentorPic":"/assets/img/mentor2.png", "mentorName":"White Root", "location":"Ann Arbor, MI, USA","industory":"Math/Physics", "profileView":"b"}
+
+
+  ]
+
 }
